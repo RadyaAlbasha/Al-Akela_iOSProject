36992842037b4a4831e779a51abb895dfe5b3e40
Radya Al-Basha <radya.albasha15@gmail.com>
2019-09-19 22:24:42 +0200

    create new project and connect it with Firebase project

---
+++ Alakela/Alakela/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alakela
+//
+//  Created by Radya Al-Basha on 9/19/19.
+//  Copyright © 2019 Radya Al-Basha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
